b95ed510f340108bb32b2a4a651af83d31498003
Ale Koko <alekoko.ruby@gmail.com>
2021-09-07 00:29:19 -0500

    Adding module WSConnector

--- wsconnector/src/main/java/com/testing/wsconnector/WSConnector.kt
+++ wsconnector/src/main/java/com/testing/wsconnector/WSConnector.kt
@@ -1,4 +1,17 @@
 package com.testing.wsconnector
 
-class WSConnector {
+import android.content.Context
+import retrofit2.Retrofit
+
+/*
+*  This class create and manage API connection
+* */
+class WSConnector(val context: Context) {
+    private lateinit var retrofitHelper: Retrofit
+
+    init {
+        retrofitHelper = Retrofit.Builder()
+            .baseUrl("")
+            .build()
+    }
 }
